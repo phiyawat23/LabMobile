9bfc1ff0649c235ae8a3626e7cf40cfe6c8b14df
10305-4 <a10305-4@iMac-1199.local>
2019-07-05 10:17:14 +0700

    Initial Lab1 05/07/62

---
+++ app/src/main/java/bank/ruts/ac/th/lab1_5_7_62/MainActivity.java
@@ -0,0 +1,13 @@
+package bank.ruts.ac.th.lab1_5_7_62;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
